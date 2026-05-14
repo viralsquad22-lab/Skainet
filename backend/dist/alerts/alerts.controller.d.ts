4b263d97dd3bdd2599ddfba3953c7506ec1c152c
alistamiento-y-mantenimiento <coopmijo.id@gmail.com>
2026-05-14 16:32:16 -0500

    fix: mover @types/node y @types/express a dependencies (fix TS2591 en Render)

--- backend/dist/alerts/alerts.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { AlertsService } from './alerts.service';
-export declare class AlertsController {
-    private readonly alertsService;
-    constructor(alertsService: AlertsService);
-    findAll(): import("./alerts.service").Alert[];
-}
--- backend/dist/machines/machines.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { MachinesService } from './machines.service';
-export declare class MachinesController {
-    private readonly machinesService;
-    constructor(machinesService: MachinesService);
-    findAll(): import("./machines.service").Machine[];
-    increment(id: string): import("./machines.service").Machine | undefined;
-    report(id: string, issue: string): import("./machines.service").Machine | undefined;
-    fix(id: string): import("./machines.service").Machine | undefined;
-}
